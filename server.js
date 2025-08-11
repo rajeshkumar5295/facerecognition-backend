327ebba6464519aefed1c007ccc35044f017fa3c
Rajesh Kumar <rajeshatka4746@gmail.com>
2025-08-11 22:39:09 +0530

    changes done

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true
 }));
 
